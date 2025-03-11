d4092ea7680ef63a869420081fd37425001c90fb
MohamedElaraby99 <mohamedlarabic6@gmail.com>
2025-03-11 21:18:34 +0200

    as

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 
